a2d720ad4e295410229b969fcf99c504e4e8e37d
Andres Agudelo <andres92-agudelo@outlook.com>
2020-05-10 01:59:40 -0500

    Se adiciona campos dependencia y tratamiento

---
+++ src/pqr/modals/addEditField/js/mixin.js
@@ -0,0 +1,42 @@
+$(function () {
+    const validateFormMixin = {
+        methods: {
+            onSubmit() {
+                let _this = this;
+                $("#form").validate({
+                    ignore: [],
+                    errorPlacement: function (error, element) {
+                        let node = element[0];
+                        if (
+                            node.tagName == 'SELECT' &&
+                            node.className.indexOf('select2') !== false
+                        ) {
+                            error.addClass('pl-3');
+                            element.next().append(error);
+                        } else {
+                            error.insertAfter(element);
+                        }
+                    },
+                    submitHandler: function (form) {
+                        $("#form_buttons").hide();
+                        $("#spiner").removeClass('d-none');
+
+                        _this.$nextTick(() => {
+                            if (_this.dataParams.isEdit) {
+                                _this.edit();
+                            } else {
+                                _this.add();
+                            }
+                        });
+                    }
+                });
+                $("#form").trigger('submit');
+            },
+            resetForm() {
+                top.closeTopModal();
+            }
+        }
+    }
+
+    top.window.validateFormMixin = validateFormMixin;
+});
