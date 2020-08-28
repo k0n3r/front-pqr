7c1959eb97a5d6aa231b49148348c4729ecb0f74
Andres Agudelo <andres92-agudelo@outlook.com>
2020-08-28 09:06:36 -0500

    Se actualiza calculo ruta rootpath

--- src/pqr/modals/addEditField/input.php
+++ src/pqr/modals/addEditField/input.php
@@ -3,7 +3,7 @@ $max_salida = 10;
 $rootPath = $ruta = '';
 
 while ($max_salida > 0) {
-    if (is_file($ruta . 'sw.js')) {
+    if (is_file($ruta . 'index.php')) {
         $rootPath = $ruta;
         break;
     }
