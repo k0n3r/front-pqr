a402e2a6f37e3222745aaacad767a5a8a529bd6d
Andres Agudelo <andres.agudelo@cerok.com>
2023-03-22 16:29:47 -0500

    Se mejora funcionalidad de validaciones adicionales, se agrega el anonimo

--- formatos/pqr/funciones.js
+++ formatos/pqr/funciones.js
@@ -14,6 +14,31 @@ function loadAnexosPqr(anexos) {
     });
 }
 
+function processField(field, applyShow = false) {
+    const sGroup = $("#group_" + field.name);
+    if (applyShow) {
+        sGroup.show();
+    }
+
+    let selector;
+    let withRules = false;
+
+    if (field.type === "Radio" || field.type === "Checkbox") {
+        selector = $("[name^='" + field.name + "']");
+        withRules = true;
+    } else {
+        selector = $("#" + field.name);
+    }
+
+    if (field.required) {
+        withRules ? selector.addClass("required") : selector.attr("required");
+        sGroup.addClass("required");
+    } else {
+        withRules ? selector.removeAttr("required") : selector.removeClass("required");
+        sGroup.removeClass("required");
+    }
+}
+
 //evento ejecutado en el adicionar
 function add(data) {
     const rcmailDataId = +top.getUrlParam('rcmailDataId', location.href);
@@ -25,6 +50,35 @@ function add(data) {
             },
             success: function (response) {
                 loadAnexosPqr(response.data.anexos_digitales);
+            }
+        });
+    }
+
+    if (+data.moreData.isEnabledAnonymous) {
+        const html = `<div class="form-group" id="group_sys_anonimo">
+            <p>
+                ¿DESEA REGISTRAR ESTA SOLICITUD COMO UNA PERSONA ANÓNIMA?
+                <input type="checkbox" name="sys_anonimo" id="sys_anonimo" value="1"/>
+            </p>
+        </div>`;
+        $("#group_dependencia").before(html);
+
+        $("#sys_anonimo").change(function () {
+            if ($(this).is(':checked')) {
+                $.each(data.moreData.fieldsWithAnonymous, function (i, field) {
+                    processField(field);
+                    const sGroup = $("#group_" + field.name);
+                    if (field.show) {
+                        sGroup.show();
+                    } else {
+                        sGroup.hide();
+                    }
+                });
+
+            } else {
+                $.each(data.moreData.fieldsWithoutAnonymous, function (i, field) {
+                    processField(field, true);
+                });
             }
         });
     }
@@ -59,9 +113,17 @@ function add(data) {
 
     addEdit(data);
 
-    // $.getScript('/views/modules/client/pqr/additionalValidations.js', function () {
-    //     additionalValidations(data);
-    // });
+    $.getScript('/views/modules/client/pqr/additionalValidations.js')
+        .done(() => {
+            try {
+                additionalValidations(data);
+            } catch (e) {
+                console.info(e.message)
+            }
+        })
+        .fail(() => {
+            console.info("No existen funciones personalizadas en el front");
+        });
 }
 
 //evento ejecutado en el editar
@@ -77,7 +139,7 @@ function edit(data) {
     addEdit(data);
 }
 
-function addEdit(data) {
+function addEdit() {
     $("#select_mensajeria option").filter(function () {
         return +$(this).data('key') === 1;
     }).remove();
@@ -92,56 +154,56 @@ function show(data) {
 
 //evento ejecutado anterior al adicionar
 function beforeSendAdd() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         resolve();
     });
 }
 
 //evento ejecutado posterior al adicionar
-function afterSendAdd(xhr) {
-    return new Promise((resolve, reject) => {
+function afterSendAdd() {
+    return new Promise((resolve) => {
         resolve();
     });
 }
 
 //evento ejecutado anterior al editar
 function beforeSendEdit() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         resolve();
     });
 }
 
 //evento ejecutado posterior al editar
-function afterSendEdit(xhr) {
-    return new Promise((resolve, reject) => {
+function afterSendEdit() {
+    return new Promise((resolve) => {
         resolve();
     });
 }
 
 //evento ejecutado anterior al devolver o rechazar
 function beforeReject() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         resolve();
     });
 }
 
 //evento ejecutado posterior al devolver o rechazar
-function afterReject(xhr) {
-    return new Promise((resolve, reject) => {
+function afterReject() {
+    return new Promise((resolve) => {
         resolve();
     });
 }
 
 //evento ejecutado anterior al confirmar o aprobar
 function beforeConfirm() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         resolve();
     });
 }
 
 //evento ejecutado posterior al confirmar o aprobar
-function afterConfirm(xhr) {
-    return new Promise((resolve, reject) => {
+function afterConfirm() {
+    return new Promise((resolve) => {
         resolve();
     });
 }
